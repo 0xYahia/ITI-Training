d70f676222dd9cc413e6db97502b7eac69a1148a
MohamedYahia831 <97320765+MohamedYahia831@users.noreply.github.com>
2023-04-02 01:30:20 +0200

    Add files via upload

---
+++ ES6/Shape/shapes.js
@@ -0,0 +1,6 @@
+import { Square, Rectangle } from "./main.js";
+
+var r1 = new Rectangle(10, 5);
+var s1 = new Square(10, 5);
+
+r1.toString();
---
+++ ES6/Swap using destructuring/script.js
@@ -0,0 +1,4 @@
+let a = 200,
+  b = 300;
+[a, b] = [b, a];
+console.log(a, b);
