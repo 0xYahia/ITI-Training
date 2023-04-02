4abb5d806336a19432a82b68f0ead8be5e9d240c
yahia <mohamedyahia207545>
2023-04-02 03:09:07 +0200

    prepare MaterialUI

---
+++ MaterialUI/src/components/MuiTypography.jsx
@@ -0,0 +1,14 @@
+import Typography from "@mui/material/Typography";
+const MuiTypography = () => {
+  return (
+    <>
+      <Typography variant="h1">MuiTypography</Typography>
+      <Typography variant="h2">MuiTypography</Typography>
+      <Typography variant="h3">MuiTypography</Typography>
+      <Typography variant="h4">MuiTypography</Typography>
+      <Typography variant="h5">MuiTypography</Typography>
+    </>
+  );
+};
+
+export default MuiTypography;
