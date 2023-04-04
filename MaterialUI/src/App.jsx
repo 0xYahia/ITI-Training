bd9e2ac32fcb775c89a3d1cc5754a35f3167b328
yahia <mohamedyahia207545>
2023-04-04 05:03:10 +0200

    Layout

--- MaterialUI/src/App.jsx
+++ MaterialUI/src/App.jsx
@@ -2,13 +2,15 @@ import MuiButtons from "./components/MuiButtons";
 import MuiTypography from "./components/MuiTypography";
 import MuiTextField from "./components/MuiTextField";
 import MuiCheckbox from "./components/MuiCheckbox";
+import MuiLayout from "./components/MuiLayout";
 const App = () => {
   return (
     <>
       {/* <MuiTypography /> */}
       {/* <MuiButtons /> */}
       {/* <MuiTextField /> */}
-      <MuiCheckbox />
+      {/* <MuiCheckbox /> */}
+      <MuiLayout />
     </>
   );
 };
